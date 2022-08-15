652988e8ab8ab8e4d09800167d2b71b00b9fcbfe
= <=>
2022-08-15 14:19:58 +0200

    add confirmation return

--- lifeguard-bot.py
+++ lifeguard-bot.py
@@ -3,9 +3,14 @@ import socket
 HOST = "185.163.124.98"  # The server's hostname or IP address
 PORT = 12754  # The port used by the server
 
+OK = 0
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b"Hello, world")
     data = s.recv(1024)
 
-print(f"Received {data!r}")
+if len(data) == 1 and data[0] == OK:
+    print("data sended succesfuly")
+else:
+    print("connexion interrupted before the end of the transfert")
