9e487234c81f9ec52de77898ef481652e5ca207e
= <=>
2022-08-15 13:43:48 +0200

    add the bot that will send the links to be analyzed

---
+++ lifeguard-bot.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = "185.163.124.98"  # The server's hostname or IP address
+PORT = 12754  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
