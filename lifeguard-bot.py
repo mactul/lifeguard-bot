aff7e0bbf60d4a66792498e7a4d26ce5507cf638
mactul <mactulgames@gmail.com>
2022-08-31 00:32:26 +0200

    create the bot

--- lifeguard-bot.py
+++ lifeguard-bot.py
@@ -1,9 +1,11 @@
 import ctypes
 import socket
+import discord
 from struct import unpack
-from threading import Thread
 from connexion_data import *
+from threading import Thread
 from central_credentials import *
+from discord_credentials import TOKEN
 
 def build_links_data(priority, password, message_id, url):
     return bytes([priority])+bytes(ctypes.c_uint64(password))+bytes(ctypes.c_uint64(message_id))+bytes(url, 'ascii')
@@ -25,17 +27,35 @@ def server():
 
                 print(message_id, password, p)
 
-th = Thread(target=server)
-th.start()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((CENTRAL_IP, UNKNOWN_LINKS_PORT))
-    s.sendall(build_links_data(1, CENTRAL_PASSWORD, 1011632414604406946, "https://cdn.discordapp.com/attachments/575715281234690048/1012823945965228064/IMG_2306.png"))
-    data = s.recv(1024)
+@client.event
+async def on_message(message:discord.Message):
+    if message.author == client.user:
+        return
+        
+    for attachment in message.attachments:
+        print(attachment.url)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((CENTRAL_IP, UNKNOWN_LINKS_PORT))
+            s.sendall(build_links_data(1, CENTRAL_PASSWORD, message.id, attachment.url))
+            data = s.recv(1)
 
-if len(data) == 1 and data[0] == TRANSFERT_OK:
-    print("data sended succesfuly")
-else:
-    print("connexion interrupted before the end of the transfert")
+            if len(data) == 1 and data[0] == TRANSFERT_OK:
+                print("data sended succesfuly")
+            else:
+                print("connexion interrupted before the end of the transfert")
+    
 
-th.join()
+
+@client.event
+async def on_ready():
+    print("bot logged in as:")
+    print(client.user.name)
+    print(client.user.id)
+    print("---------")
+
+    Thread(target=server).start()
+
+client.run(TOKEN)
